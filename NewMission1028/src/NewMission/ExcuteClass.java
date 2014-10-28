386ec93ca9b13a750b6106a7dec602d01fc2a6d8
YEOM <quasar4yjw@gmail.com>
2014-10-28 20:32:03 +0900

    ..

--- NewMission1028/src/NewMission/ExcuteClass.java
+++ NewMission1028/src/NewMission/ExcuteClass.java
@@ -21,23 +21,16 @@ public class ExcuteClass {
           .equalsIgnoreCase("add")) {
 
         list.add(command.substring(4).split(" "));
-        /*arrayContent = */
         System.out.println("저장되었습니다.");
-        /*System.out.println(list.get(0)[0]);
-        System.out.println(list.get(0)[1]);
-        System.out.println(list.get(0)[2]);
-        System.out.println(list.get(0)[3]);
-        System.out.print("0 "+ list.get(0)[0] +" "+ list.get(0)[1] + list.get(0)[2] + list.get(0)[3]);*/
 
       }//if add
       
       
-      if(command
+      else if(command
           .substring(0, 4)
           .equalsIgnoreCase("list")) {
 
 
-        /*arrayContent = */
         for (int i = 0; i < list.size(); i++){
           System.out.print(i +" "+ list.get(i)[0] 
               +" "+ list.get(i)[1] 
@@ -45,9 +38,52 @@ public class ExcuteClass {
                       +" "+ list.get(i)[3]);
         }
       }//if list
+      
+      else if(command
+          .substring(0, 4)
+          .equalsIgnoreCase("help")) {
+
+        
+        System.out.println("list");
+        System.out.println("view 인덱스");
+        System.out.println("add 이름 국어 영어 수학");
+        System.out.println("delete 인덱스");
+        System.out.println("update 인덱스");
+        System.out.println("exit");
+        
+      }//if help
+      
+      else if(command
+          .substring(0, 4)
+          .equalsIgnoreCase("view")) {
+       
+ 
+        int i = Integer.parseInt(command.substring(5, 6));
+        int total = Integer.parseInt(list.get(i)[0])
+            +Integer.parseInt(list.get(i)[0])
+            +Integer.parseInt(list.get(i)[0]);
+        float aver = total / 3f ;
+          System.out.println("인덱스: " + i);
+          System.out.println("이름: " + list.get(i)[0]);
+          System.out.println("국어: " + list.get(i)[1]);
+          System.out.println("영어: " + list.get(i)[2]);
+          System.out.println("수학: " + list.get(i)[3]);
+          System.out.println("합계: " + total);
+          System.out.println("평균: " + aver);
+      }//if view
 
       System.out.print("\n명령> ");
     }//while
 
   }
 }
+
+
+
+
+
+
+
+
+
+
